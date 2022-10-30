a22d7829c9091afc307e7fe8e8c0e70d1cf7116e
Dmitriy <Dmitriy@DESKTOP-8IVVVVB>
2022-10-30 22:42:09 +0300

    Добавьте файлы проекта.

---
+++ App2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace App2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonString = String.Empty;
+            using (StreamReader sr = new StreamReader("../../../Products.json"))
+            {
+                jsonString = sr.ReadToEnd();
+            }
+            Product[] products = JsonSerializer.Deserialize<Product[]>(jsonString);
+
+            Product maxProduct = products[0];
+            foreach (Product e in products)
+            {
+                if (e.Price>maxProduct.Price)
+                {
+                    maxProduct = e;
+                }
+            }
+            Console.WriteLine($"{maxProduct.Code} {maxProduct.Name} {maxProduct.Price}");
+            Console.Read();
+        }
+    }
+}
